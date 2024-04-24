b1c8feb9b2f34131ddbe4822d855584f84e05f6c
jaehafe <wogkdkrm112@gmail.com>
2024-04-24 19:45:20 +0900

    feat: sql추가, db 연결, Store, StoreProduct, Product 객체 생성

--- src/main/java/com/example/ordersystem/domain/Product.java
+++ src/main/java/com/example/ordersystem/domain/Product.java
@@ -1,11 +1,13 @@
 package com.example.ordersystem.domain;
 
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.math.BigDecimal;
 
+@Getter
 @Table(name = "products")
 public class Product {
     @Id
--- src/main/java/com/example/ordersystem/domain/StoreProduct.java
+++ src/main/java/com/example/ordersystem/domain/StoreProduct.java
@@ -1,4 +1,23 @@
 package com.example.ordersystem.domain;
 
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.ZonedDateTime;
+
+@Table(name = "store_products")
 public class StoreProduct {
+    @Id
+    private String storeProductId;
+
+    @Column
+    private String storeId;
+
+    @Column
+    private String productId;
+
+    @Column
+    private String stockQuantity;
 }
